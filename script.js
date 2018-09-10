da8ecf0803360cf78aafc853dddf0dfe94b81fb5
Uy Binh <uybinh@gmail.com>
2018-09-10 10:07:33 +0700

    Update form

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let button = document.querySelector("#search-box button")
+let form = document.querySelector("#search-box")
 let input = document.querySelector("#search-box input")
 
 function showImage(url) {
@@ -16,23 +16,30 @@ function showImages(urls) {
   urls.forEach(url => showImage(url))
 }
 
+function showError(err) {
+  document.querySelector("#search-result").innerHTML = err
+}
+
 function searchGiphy(query) {
   let url = `https://api.giphy.com/v1/gifs/search?q=${query}&api_key=h6uQ9JXfOEaYNUQtCfy2oiAhnsfZFQPK`
 
   return fetch(url)
     .then(response => response.json())
-    .then(json =>
-      json.data.map(function(image) {
-        return image.images.downsized.url
-      })
-    )
+    .then(json => json.data)
+    .then(data => {
+      if (data.length === 0) {
+        return Promise.reject("No Images")
+      } else {
+        return data.map(function(image) {
+          return image.images.downsized.url
+        })
+      }
+    })
 }
 
-button.addEventListener("click", () => {
+form.addEventListener("submit", event => {
+  event.preventDefault()
   const query = input.value
-  searchGiphy(query).then(urls => {
-    console.log(urls)
-    showImages(urls)
-  })
-  input.value = null
+  searchGiphy(query).then(urls => showImages(urls), err => showError(err))
+  form.reset()
 })
